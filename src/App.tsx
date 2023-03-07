47b951b66a33840210e63fae3fc2d0944ef51d86
Richard Oliver Bray <richardbray89@gmail.com>
2023-03-07 11:34:49 +0000

    chore: add css classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import HomePage from './HomePage';
+import styles from './App.module.css'
+
 export default function App() {
   return (
-    <div>
-      Hello World
+    <div class={styles.header}>
+      <nav class={styles.navigation}>
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Checkout</a>
+      </nav>
+      <header>
+        <h1>Solid Shop</h1>
+        <HomePage totalItems={340} />
+      </header>
     </div>
   );
-};
+}
