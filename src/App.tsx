422cf74c43a9d3f42e59480305028ba4ee45c610
Richard Oliver Bray <richardbray89@gmail.com>
2023-03-10 12:09:50 +0000

    chore: finished createSignal

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
 import HomePage from './HomePage';
-import styles from './App.module.css'
+import styles from './App.module.css';
+import { createSignal } from 'solid-js';
 
 export default function App() {
   const onHomePage = true;
+  const [bought, setBought] = createSignal(false);
 
-  function showAlert(text: string) {
-    alert(text)
+  const getButtonText = () => bought() ? 'Remove' : 'Buy';
+
+  function toggleBought() {
+    setBought(!bought());
   }
 
   return (
     <div class={styles.header}>
       <nav class={styles.navigation}>
-        <a href="#" classList={{ [styles.active]: onHomePage }}>Home</a>
+        <a href="#" classList={{ [styles.active]: onHomePage }}>
+          Home
+        </a>
         <a href="#">About</a>
         <a href="#">Checkout</a>
       </nav>
       <header>
         <h1>Solid Shop</h1>
         <HomePage totalItems={340} />
-        <button onClick={() => showAlert('hello')}>view</button>
+        <button onClick={toggleBought}>{getButtonText}</button>
       </header>
     </div>
   );
