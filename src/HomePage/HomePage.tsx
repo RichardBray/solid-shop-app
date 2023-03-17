70914e5669ce96388a1e9b8c987a0edca32eaaa6
Richard Oliver Bray <richardbray89@gmail.com>
2023-03-17 20:55:39 +0000

    chore: format code

--- src/HomePage/HomePage.tsx
+++ src/HomePage/HomePage.tsx
@@ -10,24 +10,25 @@ interface HomePageProps {
 }
 
 export default function HomePage({ items, getButtonText, toggleBought }: HomePageProps) {
-
   return (
     <>
       <h2>Viewing {items()?.length} Products</h2>
-<div class={styles.container}> {/* add container class here */}
-  <For each={items()}>
-    {(item) => (
-      <article class={styles.item}> {/* item class here */}
-        <img src={item.image_url} alt={item.name} />
-        <section class={styles.info}> {/* info class here */}
-          {item.name}
-          <strong>£{item.price}</strong>
-        </section>
-        <button class={styles.button} onClick={toggleBought}>{getButtonText()}</button> {/* add class here*/}
-      </article>
-    )}
-  </For>
-</div>
+      <div class={styles.container}>
+        <For each={items()}>
+          {(item) => (
+            <article class={styles.item}>
+              <img src={item.image_url} alt={item.name} />
+              <section class={styles.info}>
+                {item.name}
+                <strong>£{item.price}</strong>
+              </section>
+              <button class={styles.button} onClick={toggleBought}>
+                {getButtonText()}
+              </button>
+            </article>
+          )}
+        </For>
+      </div>
     </>
   );
 }
