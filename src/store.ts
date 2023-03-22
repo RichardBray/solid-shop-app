07e09487f143f419c70837bb4b667ae3f61ce631
Richard Oliver Bray <richardbray89@gmail.com>
2023-03-22 19:44:01 +0000

    chore: add code to prevent duplication

--- src/store.ts
+++ src/store.ts
@@ -4,5 +4,11 @@ import type { ShopItem } from './App';
 export const [boughtItems, setBoughtItems] = createStore<ShopItem[]>([]);
 
 export function addBoughtItem(item: ShopItem) {
+  // prevent adding duplicate items
+  console.log(unwrap(boughtItems).includes(item), 'boughtItems');
+  console.log(item, 'item');
+  if (unwrap(boughtItems).includes(item)) {
+    throw new Error('This item already exists');
+  }
   setBoughtItems([...boughtItems, item]);
 }
